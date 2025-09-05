73a14f7f314f8d0011db2e21a1d9dd5fc6164b77
Kassy <bongato.kc@gmail.com>
2025-09-05 15:45:50 +0800

    INITIAL LANDING PAGE

--- dishcovery/src/main.jsx
+++ dishcovery/src/main.jsx
@@ -1,10 +1,11 @@
+// src/main.jsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import LandingPage from './LandingPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <LandingPage />
   </StrictMode>,
 )
