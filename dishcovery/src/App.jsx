25d50d80fc9547decf484052720f2861a55f87d7
Kassy <bongato.kc@gmail.com>
2025-09-15 08:57:09 +0800

    UPDATED APP AND MAIN

--- dishcovery/src/App.jsx
+++ dishcovery/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import LandingPage from "./Pages/LandingPage";
 import About from "./Pages/About";
 import Explore from "./Pages/Explore";
@@ -7,15 +7,13 @@ import RecipeDetails from "./Pages/RecipeDetails";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/explore" element={<Explore />} />
-        <Route path="/recipe" element={<Recipe />} />
-        <Route path="/recipe/:id" element={<RecipeDetails />} />
-        {/* Contact is shown inside LandingPage footer; use /#contact */}
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/explore" element={<Explore />} />
+      <Route path="/recipe" element={<Recipe />} />
+      <Route path="/recipe/:id" element={<RecipeDetails />} />
+      {/* Contact is in LandingPage footer; use /#contact */}
+    </Routes>
   );
 }
